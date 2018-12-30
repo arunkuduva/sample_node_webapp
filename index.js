3b842fe3c974d114eb2364d67d790dddde5546ca
Raj <raj@stackline.com>
2018-12-29 21:47:18 -0800

    sample node web app with docker

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var request = require("request");
+var app = express();
+
+app.set("port", process.env.PORT || 8080);
+app.use(express.static(__dirname + "/public"));
+
+async function getMyIp() {
+  return new Promise(function(resolve, reject) {
+    request("https://api.ipify.org?format=json", null, function(err, resp) {
+      if (err) reject(err);
+      else resolve(resp.body);
+    });
+  });
+}
+
+app.get("/", async function(req, resp) {
+  try {
+    const ipAddress = await getMyIp();
+    resp.send(`Hello World from ${ipAddress}`);
+  } catch (e) {
+    resp.send(`Error while resolving servers ip address ${e.message}`);
+  }
+});
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running at localhost:" + app.get("port"));
+});
